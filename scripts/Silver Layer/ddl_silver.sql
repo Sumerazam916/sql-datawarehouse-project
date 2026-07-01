000ec158641937e8fe6e4b9277499f9218122441
SUMER AZAM <sumerazam916@gmail.com>
2026-07-02 00:08:43 +0530

    Update and rename ddl_silver to ddl_silver.sql

--- scripts/Silver Layer/ddl_silver.sql
+++ scripts/Silver Layer/ddl_silver.sql
@@ -1,3 +1,15 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+
 IF OBJECT_ID('Silver.crm_cust_info','U') IS NOT NULL
 	DROP TABLE Silver.crm_cust_info;
 CREATE TABLE Silver.crm_cust_info(
